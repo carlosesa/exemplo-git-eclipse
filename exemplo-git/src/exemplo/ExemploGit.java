6f4a9f9dc5ec2131e3007ee3c6683f8c1104250c
eduardo <eduardo@tjma.jus.br>
2020-04-25 11:29:35 -0300

    Estado inicial da aplicação

---
+++ exemplo-git/src/exemplo/ExemploGit.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package exemplo;
+
+/**
+ * @author Carlos Eduardo
+ *
+ */
+public class ExemploGit {
+
+	/**
+	 * 
+	 */
+	public ExemploGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Olá, Git!!!");
+	}
+
+}
